02b7b7d8d5f0a8f0711b68424baf7de26ccb12ea
Ashley Coleman <ashley78@ksu.edu>
2017-09-22 15:37:51 -0500

    Add code from tutorial

--- ListView/app/src/main/java/com/example/ashleycoleman/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/ashleycoleman/listview/MainActivity.java
@@ -8,10 +8,26 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.pokemon_list_view);
+
+        final ArrayList<Pokemon> pokemonList = Pokemon.getPokemonFromFile("pokemon.json", this);
+        String[] items = new String[pokemonList.size()];
+
+        for(int i = 0; i < pokemonList.size(); i++) {
+            Pokemon p = pokemonList.get(i);
+
+            items[i] = p.name;
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
     }
 }
